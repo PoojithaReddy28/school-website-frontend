a741732d1f9df48e22cc94927d936b6ec00e3eda
sobitd <sobitdaniel@gmail.com>
2023-03-06 16:03:02 +0300

    modify auth.service.jsx

---
+++ src/services/auth.service.jsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+const api = "http://localhost:5000/auth/";
+
+const register = (fname, lname, username, email, password) => {
+  return axios.post(api + "signup", {
+    fname,
+    lname,
+    username,
+    email,
+    password,
+  });
+};
+
+const login = (username, password) => {
+  return axios
+    .post(api + "signin", {
+      username,
+      password,
+    })
+    .then((response) => {
+      if (response.data.accessToken) {
+        localStorage.setItem("user", JSON.stringify(response.data));
+      }
+      return response.data;
+    });
+};
+
+const AuthService = {
+  register,
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default AuthService;
